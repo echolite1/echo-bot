53129e5e9dcafa5b6ef8b4db6c4b4fb018877fde
uhTVik <uhatov007@yandex.ru>
2020-03-27 20:06:24 +0100

    first manipulations

--- bot/test.js
+++ bot/test.js
@@ -1,6 +1,13 @@
 const Telegraf = require('telegraf')
 const data = require('./data')
 const TelegrafInlineMenu = require('telegraf-inline-menu')
+const Telegram = require('telegraf/telegram')
+
+
+const telegram = new Telegram(data.token, {
+  agent: null,        // https.Agent instance, allows custom proxy, certificate, keep alive, etc.
+  webhookReply: true  // Reply via webhook
+})
 
 class CustomContext extends Telegraf.Context {
   constructor (update, telegram, options) {
@@ -15,9 +22,28 @@ class CustomContext extends Telegraf.Context {
 }
 
 const bot = new Telegraf(data.token, { contextType: CustomContext })
-bot.start((ctx) => ctx.reply('Hello'))
+bot.start((ctx) => {
+	ctx.reply('Hello')
+	telegram.sendMessage(163700134, ctx.chat)// + msgInfo.message_id)
+})
 bot.help((ctx) => ctx.reply('Help message'))
 
+bot.command('oldschool', (ctx) => ctx.reply('Hello'))
+bot.command('modern', ({ reply }) => reply('Yo'))
+bot.command('hipster', Telegraf.reply('λ'))
+
+bot.command('HAHA', (ctx) => {
+	telegram.forwardMessage(163700134, 438473347, 333)
+	telegram.editMessageText(438473347, 333, 333, '.')
+ 	// telegram.deleteMessage(438473347, 329)
+ })
+
+bot.command('photo', (ctx) => {
+	telegram.forwardMessage(163700134, 438473347, 390)
+	telegram.editMessageMedia(438473347, 390, "/Users/ukhatov/Downloads/pobeda_team/Ухатов-2.jpg")//"./test.jpg")
+ 	// telegram.deleteMessage(438473347, 329)
+ })
+// bot.command('HAHA', (ctx) => )
 
 const menu = new TelegrafInlineMenu(ctx => `Hey ${ctx.from.first_name}!`)
 menu.setCommand('play')
@@ -27,6 +53,8 @@ menu.simpleButton('I am excited!', 'a', {
 menu.simpleButton('I am not excited!', 'b', {
   doFunc: ctx => ctx.reply('Coronatime!')
 })
+
+
 
 bot.use(menu.init())
  
