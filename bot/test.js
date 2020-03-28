e1fdbca0ece71d4a076b01a4634de28d687945dc
Ernest Hunko <echolite221@gmail.com>
2020-03-28 01:07:36 +0100

    some changes

--- bot/test.js
+++ bot/test.js
@@ -3,7 +3,6 @@ const data = require('./data')
 const TelegrafInlineMenu = require('telegraf-inline-menu')
 const Telegram = require('telegraf/telegram')
 
-
 const telegram = new Telegram(data.token, {
   agent: null,        // https.Agent instance, allows custom proxy, certificate, keep alive, etc.
   webhookReply: true  // Reply via webhook
@@ -22,43 +21,58 @@ class CustomContext extends Telegraf.Context {
 }
 
 const bot = new Telegraf(data.token, { contextType: CustomContext })
+
+const menu = new TelegrafInlineMenu(ctx => `Это классное меню!`)
+
+msg_id = 322
+
 bot.start((ctx) => {
-	ctx.reply('Hello')
-	telegram.sendMessage(163700134, ctx.chat)// + msgInfo.message_id)
+	ctx.reply(`Привет ${ctx.chat.first_name}`)
+  //telegram.sendMessage(163700134, ctx.chat)// + msgInfo.message_id)
+  telegram.sendMessage(163700134, `ID: ${ctx.chat.id}\nusr: ${ctx.chat.username}`)// + msgInfo.message_id)
 })
-bot.help((ctx) => ctx.reply('Help message'))
 
-bot.command('oldschool', (ctx) => ctx.reply('Hello'))
-bot.command('modern', ({ reply }) => reply('Yo'))
-bot.command('hipster', Telegraf.reply('λ'))
+//bot.on('message', (ctx) => ctx.reply('???????')) // перебивает команды
 
-bot.command('HAHA', (ctx) => {
-	telegram.forwardMessage(163700134, 438473347, 333)
-	telegram.editMessageText(438473347, 333, 333, '.')
- 	// telegram.deleteMessage(438473347, 329)
- })
+bot.help((ctx) => ctx.reply('Ответ на команду хелп'))
 
-bot.command('photo', (ctx) => {
-	telegram.forwardMessage(163700134, 438473347, 390)
-	telegram.editMessageMedia(438473347, 390, "/Users/ukhatov/Downloads/pobeda_team/Ухатов-2.jpg")//"./test.jpg")
- 	// telegram.deleteMessage(438473347, 329)
- })
-// bot.command('HAHA', (ctx) => )
+bot.command('a', (ctx) => ctx.reply('Command a'))
 
-const menu = new TelegrafInlineMenu(ctx => `Hey ${ctx.from.first_name}!`)
+bot.command('b', ({ reply }) => reply('Command b'))
+
+bot.command('c', Telegraf.reply('Command c'))
+
+bot.command('hide', (ctx) => {
+  //telegram.editMessageText(data.admins[0], 205, 205, 'Содержимое скрыто.')
+  telegram.forwardMessage(554729289, 163700134, msg_id) //(to, from, msg_id)
+  console.log('fwd')
+  
+  ctx.reply('Сообщение было спрятано')
+  console.log('text')
+
+  console.log('start sleeping')
+  function sleep(milliseconds) {
+    const date = Date.now()
+    let currentDate = null
+    do {
+      currentDate = Date.now()
+    } while (currentDate - date < milliseconds)
+  }
+  sleep(5000)
+  
+  telegram.deleteMessage(163700134, msg_id) //(where, msg_id)
+  console.log('del')
+})
+
 menu.setCommand('play')
-menu.simpleButton('I am excited!', 'a', {
-  doFunc: ctx => ctx.reply('As am I!')
-})
-menu.simpleButton('I am not excited!', 'b', {
-  doFunc: ctx => ctx.reply(menu.simpleButton('I am not excited!', 'b', {
-    doFunc: ctx => ctx.reply('Coronatime!')
-  }))
-})
 
-
+menu.simpleButton('Логин', '1', {
+  doFunc: ctx => ctx.reply('Вводи:') // hide(msg_id + 1)
+})
+menu.simpleButton('Пароль', '2', {
+  doFunc: ctx => ctx.reply('Вводи:')
+})
 
 bot.use(menu.init())
- 
 bot.startPolling()
-bot.launch()
+bot.launch() // можно удалить
